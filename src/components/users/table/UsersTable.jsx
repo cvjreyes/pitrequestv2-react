9af3935a26fd03331e55cc23af8e8567579848fb
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-05-29 17:48:17 +0200

    Data from table shown

--- src/components/users/table/UsersTable.jsx
+++ src/components/users/table/UsersTable.jsx
@@ -4,30 +4,16 @@ import { jsx } from "@emotion/react";
 import { useDemoData } from "@mui/x-data-grid-generator";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { AiOutlineEdit } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { client } from "../../../helpers/config";
 
 export default function UsersTable() {
-  const rows = [
-    {
-      id: 1,
-      col1: "Sean Saez Fuller",
-      col2: "sean.saez-fuller@technipenergies.com",
-    },
-    {
-      id: 2,
-      col1: "Ignacio Gonzalez",
-      col2: "ignacio.gonzalez@external.technipenergies.com",
-    },
-    {
-      id: 3,
-      col1: "Jorge Reyes",
-      col2: "jorge.reyes-sztayzel@technipenergies.com",
-    },
-  ];
+  const [rows, setRows] = useState([]);
 
   const columns = [
-    { field: "col1", headerName: "Name", width: 400 },
-    { field: "col2", headerName: "Email", width: 400 },
-    { field: "col3", headerName: "Projects", width: 200 },
+    { field: "col1", headerName: "Name", width: 300 },
+    { field: "col2", headerName: "Email", width: 450 },
+    { field: "col3", headerName: "Projects", width: 450 },
     { field: "col4", headerName: "Roles", width: 200 },
     {
       field: "col5",
@@ -46,6 +32,27 @@ export default function UsersTable() {
     rowLength: 4,
     maxColumns: 6,
   });
+
+  const getUsers = async () => {
+    const response = await client.get("/users/projects/roles");
+    const users = response.data;
+
+    const formattedRows = users.map((user) => ({
+      id: user.id,
+      col1: user.name,
+      col2: user.email,
+      col3: user.ProjectUsers.map(
+        (projectUser) => projectUser.Project.name
+      ).join(", "),
+      col4: user.UsersRol.map((userRol) => userRol.rol.name).join(", "),
+    }));
+
+    setRows(formattedRows);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
     <div className="container-table">
